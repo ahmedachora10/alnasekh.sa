2b9d0426c0c4d7696dc459a31466e40d521764a8
ahmedachora10 <gjr8195@gmail.com>
2023-11-23 14:17:59 +0100

    make ar_slug to handle arabic letters

--- app/Utils/functions.php
+++ app/Utils/functions.php
@@ -19,3 +19,25 @@ if (! function_exists('setting')) {
         return is_null($value) ? value($default) : $value;
     }
 }
+
+if(!function_exists('ar_slug')) {
+
+    function ar_slug(string $string, string $separator = '-') {
+        if (is_null($string)) {
+            return "";
+        }
+
+        $string = trim($string);
+
+        $string = mb_strtolower($string, "UTF-8");;
+
+        $string = preg_replace("/[^a-z0-9_\sءاأإآؤئبتثجحخدذرزسشصضطظعغفقكلمنهويةى]#u/", "", $string);
+
+        $string = preg_replace("/[\s-]+/", " ", $string);
+
+        $string = preg_replace("/[\s_]/", $separator, $string);
+
+        return $string;
+    }
+
+}
