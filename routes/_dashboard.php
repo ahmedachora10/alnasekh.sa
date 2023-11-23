5e37abb7b8bca105ee912eeae3eb2c47d728da64
ahmedachora10 <gjr8195@gmail.com>
2023-11-23 11:39:25 +0100

    add dashboard routes

---
+++ routes/_dashboard.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\Admin\MediaController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\SettingController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware(['auth'])->group(function ()
+{
+    Route::controller(SettingController::class)
+    ->prefix('settings')->name('settings.')
+    ->group(function ()
+    {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+    });
+
+    Route::controller(MediaController::class)
+    ->prefix('images')->name('images.')
+    ->group(function ()
+    {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->middleware('verified')->name('dashboard');
+
+    Route::get('/users', [UserController::class, 'index'])->name('admin.users');
+});
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
