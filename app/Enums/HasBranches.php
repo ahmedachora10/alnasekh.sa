3b188e363f7171ff847e68f8df9d9ea7518f8195
ahmedachora10 <gjr8195@gmail.com>
2023-11-24 19:48:03 +0100

    add has branches enum

---
+++ app/Enums/HasBranches.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Enums;
+
+enum HasBranches:int {
+    case No = 0;
+    case Yes = 1;
+
+    public function name() {
+        return match ($this) {
+            self::Yes => trans('common.yes'),
+            self::No => trans('common.no'),
+            default => '',
+        };
+    }
+
+    public function color() {
+        return match ($this) {
+            self::Yes => 'success',
+            self::No => 'secondary',
+        };
+    }
+
+    public static function values() {
+        $values = [];
+
+        foreach (self::cases() as $obj) {
+            $values[] = $obj->value;
+        }
+
+        return $values;
+    }
+}
