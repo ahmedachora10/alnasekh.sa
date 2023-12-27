29f54b279d59d6dc600a2406fe289418bb099a0e
ahmedachora10 <gjr8195@gmail.com>
2023-12-27 12:54:20 +0100

    update

--- resources/views/mails/send-reminder.blade.php
+++ resources/views/mails/send-reminder.blade.php
@@ -79,48 +79,17 @@
         </tr><!-- Outer-Table -->
     </table>
     <div style="padding:20px 30px; background-color: white; font-size: 13px;">
-        <span style="display: block; margin-bottom: 6px; color: #34221e; font-weight:700; text-align:right">
+        <span style="display: block; margin-bottom: 6px; color: #34221e; font-weight:700">
             <img src="{{ asset('assets/icons/map.svg') }}" style="width: 14px;" alt="">
-            {{-- <?xml version="1.0" encoding="iso-8859-1"?>
-            <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-            <svg fill="#ca9475" height="100%" width="14px" version="1.1" id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"
-                xml:space="preserve">
-                <g>
-                    <g>
-                        <path d="M256,0C154.739,0,72.348,82.386,72.348,183.652c0,70.241,18.514,94.635,169.771,320.929
-   C245.217,509.217,250.424,512,256,512s10.782-2.783,13.881-7.418c151.251-226.216,169.771-251.423,169.771-320.929
-   C439.652,82.386,357.261,0,256,0z M256,267.13c-46.032,0-83.478-37.446-83.478-83.478c0-46.032,37.446-83.478,83.478-83.478
-   s83.478,37.446,83.478,83.478C339.478,229.684,302.032,267.13,256,267.13z" />
-                    </g>
-                </g>
-            </svg> --}}
-
             <b>
                 المملكة العربية السعودية - الفرع
                 الرئيسي: حائل - حي المطار
             </b>
         </span>
 
-        <span style="display: block; color: #34221e; font-weight:700; text-align:right">
+        <span style="display: block; color: #34221e; font-weight:700">
             <img src="{{ asset('assets/icons/envelope.svg') }}" style="width: 12px; position: relative; top: 3px"
                 alt="">
-
-            {{-- <?xml version="1.0" encoding="utf-8"?>
-            <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-            <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-            <svg version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink" style="position: relative; top: 3px;" width="12px"
-                height="100%" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
-                <g>
-                    <path fill="#ca9475" d="M43.416,35.812c-0.448-0.322-0.55-0.947-0.228-1.396c0.321-0.448,0.947-0.55,1.396-0.228L64,48.168V15.475
-  L32.597,38.803C32.419,38.935,32.21,39,32,39s-0.419-0.065-0.597-0.197L0,15.475v32.693l19.416-13.979
-  c0.449-0.321,1.073-0.22,1.396,0.228c0.322,0.448,0.221,1.073-0.228,1.396L0,50.632V52c0,2.211,1.789,4,4,4h56c2.211,0,4-1.789,4-4
-  v-1.368L43.416,35.812z" />
-                    <path fill="#ca9475"
-                        d="M32,36.754l32-23.771V12c0-2.211-1.789-4-4-4H4c-2.211,0-4,1.789-4,4v0.982L32,36.754z" />
-                </g>
-            </svg> --}}
 
             <b class="padding-right:5px">{{ setting('email') }}</b>
         </span>
