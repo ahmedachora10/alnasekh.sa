b5b7389d1870877dc028e4af59cb3375aa09ddd9
ahmedachora10 <gjr8195@gmail.com>
2024-02-04 18:56:54 +0100

    update job

--- app/Jobs/DateReminder.php
+++ app/Jobs/DateReminder.php
@@ -24,7 +24,7 @@ class DateReminder
     /**
      * Execute the job.
      */
-    public function __invoke(): void
+    public function __invoke()
     {
         $admins = User::whereHasRole('admin')->get();
 
