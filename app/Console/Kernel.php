0fd087ee5425396d1d75add48bd431069594e65c
ahmedachora10 <gjr8195@gmail.com>
2024-02-09 02:25:48 +0100

    update reminder notification logic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,23 +2,10 @@
 
 namespace App\Console;
 
-use App\Enums\Status;
 use App\Jobs\DateReminder;
-use App\Models\BranchCertificate;
-use App\Models\BranchEmployee;
-use App\Models\BranchRecord;
-use App\Models\BranchSubscription;
-use App\Models\Corp;
-use App\Models\CorpBranch;
-use App\Models\CorpBranchMonthlyQuarterlyUpdate;
-use App\Models\CorpBranchRegistry;
-use App\Models\Registry;
-use App\Models\User;
-use App\Notifications\UserActionNotification;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Notification;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -28,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(new DateReminder)->everyFiveMinutes();
+        $schedule->call(new DateReminder)->everyMinute();
     }
 
     /**
