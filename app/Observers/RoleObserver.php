63666d36131bd91fbea2ac6fa1823315c837bd77
ahmedachora10 <gjr8195@gmail.com>
2024-02-24 16:11:42 +0100

    update delete notification action

--- app/Observers/RoleObserver.php
+++ app/Observers/RoleObserver.php
@@ -3,15 +3,18 @@
 namespace App\Observers;
 
 use App\Models\Role;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
 
 class RoleObserver
 {
     public function updated(Role $role) {
-        Cache::forget('user-permissions');
+        // Cache::forget('user-permissions');
+        Artisan::call('optimize:clear');
     }
 
     public function deleted(Role $role) {
-        Cache::forget('user-permissions');
+        // Cache::forget('user-permissions');
+        Artisan::call('optimize:clear');
     }
 }
